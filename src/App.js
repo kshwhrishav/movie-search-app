0bb5cdb70786aafa64733cc0f3ca4453ea9d35b9
kshwhrishav <kshwhrishav@gmail.com>
2024-11-16 18:52:40 +0530

    basic-structure-created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieSearch from './MovieSearch';
+
+function App() {
+  return (
+    <div className="App">
+      <MovieSearch />
+    </div>
+  );
+}
+
+export default App;
